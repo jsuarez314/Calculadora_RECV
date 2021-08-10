55594227703e8e1562a1f593d89867c8229b9b8b
AnaMontana10 <42219647+AnaMontana10@users.noreply.github.com>
2021-08-10 18:25:11 -0500

    Update calculadora.py
    Serie Fibonacci

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,35 @@
 import numpy as np
+
+def fibonacci(a):
+
+	serie =  0
+	s = 0
+	f0 ,f1  = 0,1  #primeros dos terminos
+
+	
+
+	#verificar que sea mayor  a  0
+
+	if a == 0:
+
+		print("El  número debe ser mayor a 0")
+
+	#verificar que no sea un numero decinal
+
+	elif a / a != 1:
+		print("utilice numero entero")
+
+
+	elif a == 1:
+		print(f0)
+
+	else:
+
+		while serie < a:  
+			print(f0)
+			s=  f0  + f1
+			f0  = f1
+			f1 = s
+			serie =  serie + 1 
+
+ 
