3ce649418cbc4ea8e4777046fbe7e037bf1a011d
JuanIMartinezB <88747334+JuanIMartinezB@users.noreply.github.com>
2021-08-23 18:54:26 -0500

    Máximo de tres números por Martínez Bustamente.
    Máximo de tres números por Martínez Bustamente.

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,14 @@
 import numpy as np
 
 
+def maximo(a,b,c):
+    n = [a,b,c]
+    mayor = n[0]
+    for i in n:
+        if i > mayor:
+            mayor = i
+    return mayor
+
 def raiz3(a):
   raiz = np.cbrt(a)
   return raiz
